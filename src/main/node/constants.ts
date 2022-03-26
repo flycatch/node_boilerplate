83a22672a6b4d4d8bc24b6269ac642f429382d2b
kareem <abdul.k@flycatchtech.com>
2022-03-26 22:34:03 +0530

    feat: added constants file

---
+++ src/main/node/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+    INIT_COMMAND_DESC: 'initialize current directory for a node web api project',
+    INIT_INSTALL_FLAG_DESC: 'install packages via npm',
+    INIT_GIT_FLAG_DESC: 'initialize as a new git repository',
+    INIT_YES_FLAG_DESC: 'use default options',
+    INIT_TARGET_FLAG_DESC: 'target folder to generate project',
+
+    HELP_COMMAND_DESC: 'Get help for the cli and commands',
+};
